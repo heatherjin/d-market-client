56e1d48c18b9c52591814e5629ead6a4e1f1bbb9
dahye <dhjin1@naver.com>
2023-03-11 19:31:21 +0900

    API_URL 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
 //React 프로젝트에서 쓰이는 다양한 변수 선언, 상수라 대문자로 적음, 한개가 아니라서 defult 안씀
-export const API_URL = "http://localhost:8080";
+export const API_URL = process.env.NODE_ENV ==='production' ? "https://d-market-server.fly.dev" : "http://localhost:8080";
